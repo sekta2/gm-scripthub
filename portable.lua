a236a129481a2f65df7333d2825864bc0a8fb005
Mihail <akovlevmihail535@gmail.com>
2022-08-31 01:16:37 +0400

    Update portable.lua

--- portable.lua
+++ portable.lua
@@ -1,1 +1,8 @@
--- This version of ScriptHub is meant to be run via [lua_run_cl http.fetch("")]
+-- This version of ScriptHub is meant to be run via [lua_run_cl http.fetch("https://raw.githubusercontent.com/sekta2/gm-scripthub/main/portable.lua",function(content) RunString(content) end)]
+-- or for [lua_openscript_cl portable.lua] if you have this script on [lua/]
+
+
+scripthub = {} or scripthub
+
+
+scripthub.window =
